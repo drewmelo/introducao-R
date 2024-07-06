b4ef501d929ffad912225f0a444904729a684d73
André <andremelopix@hotmail.com>
2024-07-06 14:22:38 -0300

    script modificado

--- script.R
+++ script.R
@@ -1,3 +1,5 @@
+library(tidyverse)
+
 
 # DIRETÓRIO ---------------------------------------------------------------
 
@@ -208,7 +210,7 @@ data(package = "datasets")
 
 install.packages("devtools")
 
-devtools::install_github("drewmelo/beautyxtrar", force = T)
+# devtools::install_github("drewmelo/beautyxtrar", force = T)
 
 library(dados)
 
@@ -250,3 +252,305 @@ wr(dados, file = "dados/dados.xlsx", rowNames = FALSE)
 library(readxl)
 
 dados_importado <- readxl::read_excel(path = "dados/dados.xlsx")
+
+## PRÁTICAS NO R -----------------------------------------------------------
+
+library(tidyverse)
+
+dados <- tibble(
+  nome = c("Darth Vader", "Luke Skywalker", "Leia Organa", "Obi-Wan Kenobi"),
+  altura = c(202, 172, 150, 182),
+  massa = c(136, 77, 49, 77),
+  genero = c("Masculino", "Masculino", "Feminino", "Masculino")
+)
+
+dados
+
+lista <- list(
+  dados = dados,
+  hobbies = c("Leitura", "Esportes", "Música"),
+  matriz = matrix(1:9, nrow = 3, dimnames = list(c('A', 'B', 'C'),
+                                                 c('A', 'B', 'C'))),
+  matriz_empilhada = array(c(1:12), dim = c(2, 3, 2)),
+  cars = head(cars, 5),
+  mtcars = head(mtcars, 5)
+)
+
+lista$dados[[1]]
+
+a <- c("Darth Vader",    "Luke Skywalker", "Leia Organa",    "Obi-Wan Kenobi")
+
+lista[[1]][1 , ]
+
+lista$dados[lista$dados$altura > 170]
+
+# Outra alternativa
+lista$dados[lista$dados$nome == "Luke Skywalker", "genero"] <- "Feminino"
+
+library(readxl)
+dados <- read_excel("C:/users/andre/Downloads/dados.xlsx")
+
+arrange(.data = dados, genero)
+
+# IF e ELSE ---------------------------------------------------------------
+
+library(tidyverse)
+
+nota <- 6
+
+if (nota >= 7) {
+  print("Parabéns! Você foi aprovado")
+} else {
+  print("Você não passou!")
+}
+
+nota <- 4
+
+if (nota >= 7) {
+  cat("Parabéns! passou.")
+} else if (nota >= 4 & nota < 7) {
+  cat("Você está de recuperação.")
+} else {
+  cat("Reprovado!")
+}
+
+
+# Diferenças entre operadores lógicos (& e &&) e (| e ||) -----------------
+
+dados2 <- tibble(
+  a = c(1, 0, 1),
+  b = c(0, 0, 1),
+  c = c("João", "João", "João"),
+  d = c("João", "João", "João")
+)
+
+dados2 |>
+  filter(a & b)
+
+dados2 |>
+  filter(a && b)
+
+dados2 |>
+  filter(a | b)
+
+dados2 |>
+  filter(a || b)
+
+# Aplicando && e || em funções --------------------------------------------
+
+a <- T
+b <- F
+
+if (a && b) {
+  paste("Ambos são TRUE")
+} else {
+  paste("Um ou ambos são FALSE")
+}
+
+if (a || b) {
+  cat("Pelo menos um entre 'a' e 'b' são TRUE")
+} else {
+  cat("Ambos são FALSE")
+}
+
+c <- T
+
+if (a && c(c || d)) {
+  cat("'a' é verdadeiro e pelo menos um entre 'b' e 'c' é verdadeiro")
+} else if (!a && c(b && c)) {
+  cat("'a' é falso e b e c são verdadeiros")
+} else {
+  cat("Nenhuma das condições foram atendidas")
+}
+
+# Opções alternativas
+# ifelse e if_else
+
+# Função Switch -----------------------------------------------------------
+
+dia <- 3
+
+switch (dia,
+        "domingo" = {
+          cat("Hoje é domingo.")
+        },
+        "segunda" = {
+          cat("Hoje é segunda")
+        },
+        "terça" = {
+          cat("Hoje é terça")
+        }
+)
+
+# Função for
+for (i in 1:10) {
+  print(c("Estou em um loop", i))
+}
+
+max(cars$speed) - min(cars$speed)
+
+veloc <- max(cars$speed)
+
+# Definir os objetos do loop
+veloc_min <- -99999999999999999
+veloc_min <- -Inf
+
+for (i in veloc) {
+  if (i > veloc_min) veloc_min = i
+}
+
+#
+
+# Função repeat -----------------------------------------------------------
+# Inicializando uma variável
+contador <- 1
+
+# Loop repeat
+repeat {
+  print(contador)
+  contador <- contador + 1
+  if (contador > 5) {
+    break
+  }
+}
+
+n = 5
+result = 1
+
+# Loop repeat
+repeat {
+  result <-  result * n
+  n <- n - 1
+  if ( n <= 1) {
+    break
+  }
+}
+
+cat("A fatorial de 5 é:", result)
+
+# Amostra aleatória
+set.seed(125)
+
+sample(x = 1:10, size = 20, replace = T)
+
+x <- factor(sample(letters[1:10], size = 20, replace = T))
+
+y <- ifelse(x %in% c("a", "b", "c"), yes = x, no = NA)
+
+y_novo <- if_else(x %in% c("a", "b", "c"), true = x, false = NA)
+
+# Função para calcular o IMC ----------------------------------------------
+
+dados_sw <- dados::dados_starwars |>
+  select(1:5)
+
+calculo_imc <- function(data, peso_col, altura_col) {
+  # Tornando o imc (vetor) númerico
+  imc <- numeric(length = nrow(data))
+
+  # Verificar se há NA nas colunas peso e altura (argumentos) -- Somente observações completas
+  dados_full <- complete.cases(data[[peso_col]],  data[[altura_col]])
+
+  imc[dados_full] <- data[[peso_col]][dados_full]  / ( data[[altura_col]][dados_full] / 100 )^2
+
+  categorias <- character(length = length(imc))
+
+  # Categorizando os resultados
+  categorias[imc < 18.5] <- "Abaixo do peso"
+  categorias[imc <= 18.5 & imc < 25] <- "Peso normal"
+  categorias[imc >= 25] <- "Acima do peso"
+
+  # Adicionando NA's
+  categorias[!dados_full] <- NA
+
+  return(categorias)
+}
+
+# Aplicando essa função aos nossos dados
+dados_sw$imc_categorizado <- calculo_imc(data = dados_sw,
+                                         altura_col = "altura",
+                                         peso_col = "massa")
+
+
+# Aplicando a função a outros dados
+dados_pessoas <- dados::pessoas |>
+  select(sobrenome, altura = peso, peso = altura)
+
+dados_pessoas$novo_imc <- calculo_imc(altura_col = "altura",
+                                      peso_col = "peso", data = dados_pessoas)
+
+write.csv(x = dados_pessoas, file = "dados/dados_pessoas.csv", row.names = F)
+
+# Importando os mesmos dados
+dados <- read_csv(file = "dados/dados_pessoas.csv")
+
+# ANÁLISE ESTATÍSTICA -----------------------------------------------------
+
+as_tibble(pessoas) |>
+  select(nome, peso, altura) |>
+  mutate(categoria = calcular_imc(data = pessoas,
+                                  peso_col = "peso",
+                                  altura_col = "altura")) |>
+  head(n = 5)
+
+
+# ANÁLISE ESTATÍSTICA -----------------------------------------------------
+dados2 <- dados |>
+  mutate(novo_imc = factor(novo_imc,
+                           levels = c("Abaixo do peso",
+                                      "Peso normal",
+                                      "Acima do peso"), ordered = TRUE),
+         imc = peso / (altura / 100)^2 )
+
+v <- c(rep(10, 8), 1:15, 20:40)
+
+uniquev <- unique(v)
+
+# Função para calcular a moda
+calcular_moda <- function(v) {
+  uniqv <- unique(v)
+  uniqv[which.max(tabulate(match(v, uniqv)))]
+}
+
+calcular_moda(v)
+
+# Função para calcular todas as operações estatísticas em uma coluna do dataset starwars
+resumo_estatistico <- function(df, coluna) {
+  media <- mean(df[[coluna]], na.rm = TRUE)
+  soma <- sum(df[[coluna]], na.rm = TRUE)
+  variancia <- var(df[[coluna]], na.rm = TRUE)
+  desvio_padrao <- sd(df[[coluna]], na.rm = TRUE)
+  moda <- calcular_moda(df[[coluna]])
+
+  return(list(
+    media = media,
+    soma = soma,
+    variancia = variancia,
+    desvio_padrao = desvio_padrao,
+    moda = moda
+  ))
+}
+
+# Exemplo de uso
+resumo_altura <- resumo_estatistico(dados2, "altura")
+
+summary(dados2)
+
+dados2 |>
+  ggplot(aes(x = altura, y = massa)) +
+  geom_point() +
+  scale_y_continuous(limits = c(0, 159))
+
+# Histograma da altura
+hist(dados$altura, breaks = 10, col = "lightblue3", main = "Histograma de Altura", xlab = "Altura (cm)", ylab = "Frequência")
+
+# Boxplot da massa
+boxplot(dados$massa, main = "Boxplot de Massa", ylab = "Massa (kg)", col = "lightblue")
+
+
+# Links -------------------------------------------------------------------
+# Banco Central do Brasil
+"https://www3.bcb.gov.br/sgspub/localizarseries/localizarSeries.do?method=prepararTelaLocalizarSeries"
+
+# Inflação
+"https://www.ibge.gov.br/estatisticas/economicas/precos-e-custos/9256-indice-nacional-de-precos-ao-consumidor-amplo.html?=&t=series-historicas"
